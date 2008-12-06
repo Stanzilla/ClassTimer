090385d15c6709bb6b964099d9b5479456cd9109
dessa <Dessa@users.noreply.github.com>
2008-12-06 12:02:16 +0000

    add Holy Vengeance debuff, cosmetic fixes

--- Bars/Paladin.lua
+++ Bars/Paladin.lua
@@ -16,7 +16,7 @@ return {
 		GetSpellInfo(25898), -- Greater Blessing of Kings
 		GetSpellInfo(25782), -- Greater Blessing of Might
 		GetSpellInfo(25899), -- Greater Blessing of Sanctuary
-		(GetSpellInfo(25894)), -- Greater Blessing of Wisdom --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
+		(GetSpellInfo(25894)), -- Greater Blessing of Wisdom -- Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Buffs = {
 		GetSpellInfo(31884), -- Avenging Wrath
@@ -24,15 +24,15 @@ return {
 		GetSpellInfo(642), -- Divine Shield
 		GetSpellInfo(20177), -- Reckoning
 		GetSpellInfo(25780), -- Righteous Fury
-		GetSpellInfo(53601), --Sacred Shield
-		GetSpellInfo(53486), --The Art of War
-		(GetSpellInfo(20925)), -- Holy Shield --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
+		GetSpellInfo(53601), -- Sacred Shield
+		GetSpellInfo(53486), -- The Art of War
+		(GetSpellInfo(20925)), -- Holy Shield -- Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Judgements = {
 		GetSpellInfo(53407), -- Judgement of Justice
 		GetSpellInfo(20271), -- Judgement of Light
-		GetSpellInfo(53671), --Judgements of the Pure
-		(GetSpellInfo(53408)), -- Judgement of Wisdom --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
+		GetSpellInfo(53671), -- Judgements of the Pure
+		(GetSpellInfo(53408)), -- Judgement of Wisdom -- Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Seals = {
 		GetSpellInfo(53720), -- Seal of the Martyr
@@ -43,26 +43,27 @@ return {
 		GetSpellInfo(21084), -- Seal of Righteousness
 		GetSpellInfo(31801), -- Seal of Vengeance
 		GetSpellInfo(53736), -- Seal of Corruption
-		(GetSpellInfo(20166)), -- Seal of Wisdom --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
+		(GetSpellInfo(20166)), -- Seal of Wisdom -- Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Stuns = {
 		GetSpellInfo(853), -- Hammer of Justice
-		(GetSpellInfo(20066)), -- Repentance --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
+		(GetSpellInfo(20066)), -- Repentance -- Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Misc = {
-		GetSpellInfo(53380), --Righteous Vengeance
+		GetSpellInfo(53380), -- Righteous Vengeance
 		GetSpellInfo(31935), -- Avenger's Shield
 		GetSpellInfo(26573), -- Consecration
 		GetSpellInfo(31842), -- Divine Illumination
-		GetSpellInfo(53563), --Beacon of Light
+		GetSpellInfo(53563), -- Beacon of Light
 		GetSpellInfo(31833), -- Light's Grace
 		GetSpellInfo(53672), -- Infusion of Light
 		GetSpellInfo(20127), -- Redoubt
 		GetSpellInfo(10326), -- Turn Evil
-		GetSpellInfo(20049), --Vengeance
+		GetSpellInfo(20049), -- Vengeance
 		GetSpellInfo(20335), -- Heart of the Crusader
 		GetSpellInfo(53380), -- Righteous Vengeance
-		(GetSpellInfo(9452)), -- Vindication --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
+		GetSpellInfo(31803), -- Holy Vengeance
+		(GetSpellInfo(9452)), -- Vindication -- Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	}	
 }
 end
