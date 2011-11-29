5f62667d29f1ac98777366365a624d885cbba18a
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2011-11-29 16:21:24 +0000

    toc update and something else, probably shaman related!

--- Bars/Shaman.lua
+++ Bars/Shaman.lua
@@ -7,21 +7,31 @@ return {
 	Buffs= {
 		GetSpellInfo(16176), -- Ancestral Healing
 		GetSpellInfo(30160), -- Elemental Devastation
+		GetSpellInfo(64701), -- Elemental Mastery (Haste + Damage Buff)
+		GetSpellInfo(16166), -- Elemental Mastery (Instant)
 		GetSpellInfo(29062), -- Eye of the Storm
 		GetSpellInfo(29206), -- Healing Way
 		GetSpellInfo(30823), -- Shamanistic Rage
---		GetSpellInfo(8033), -- Frostbrand Weapon
 --		GetSpellInfo(51528), -- Maelstrom Weapon
 --		GetSpellInfo(51730), -- Earthliving Weapon
+--		GetSpellInfo(8024), -- Flametongue Weapon
 --		GetSpellInfo(8232), -- Windfury Weapon
+		GetSpellInfo(16246), -- Clearcasting
+		GetSpellInfo(73683), -- Unleash Flame
+		GetSpellInfo(73681), -- Unleash Wind
 		GetSpellInfo(51945), -- Earthliving
 		GetSpellInfo(55198), -- Tidal Force
+		GetSpellInfo(79206), -- Spiritwalker's Grace
 		GetSpellInfo(17364), -- Stormstrike
 		GetSpellInfo(61295), -- Riptide
 		GetSpellInfo(51562), -- Tidal Waves
+		GetSpellInfo(131), -- Water Breathing
+		GetSpellInfo(546), -- Water Walking
 		(GetSpellInfo(30802)), -- Unleashed Rage --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Shocks = {
+		GetSpellInfo(73684), -- Unleash Earth
+		GetSpellInfo(73682), -- Unleash Frost
 		GetSpellInfo(8042), -- Earth Shock
 		GetSpellInfo(8050), -- Flame Shock
 		(GetSpellInfo(8056)), -- Frost Shock --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
