5733bfd74b0cd15642fdb165803b0d86e5f089bb
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2013-03-06 01:48:28 +0000

    this is emberassing

--- Bars/Warlock.lua
+++ Bars/Warlock.lua
@@ -43,7 +43,7 @@ return {
 		GetSpellInfo(47960), -- Shadowflame
 		GetSpellInfo(17962), -- Conflagrate
 		GetSpellInfo(689), -- Drain Life
-		GetSpellInfo(80240). -- Havor
+		GetSpellInfo(80240), -- Havor
 		(GetSpellInfo(30108)), -- Unstable Affliction --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Misc = {
