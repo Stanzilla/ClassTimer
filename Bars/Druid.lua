c0da1eb68b6987a52ffe4cbc09bf0ac10c3483bc
Benjamin Staneck <staneck@gmail.com>
2016-08-14 06:16:13 +0200

    update Druid spells

--- Bars/Druid.lua
+++ Bars/Druid.lua
@@ -11,16 +11,18 @@ return {
 		GetSpellInfo(29166), -- Innervate
 		GetSpellInfo(33763), -- Lifebloom
 		GetSpellInfo(8936), -- Regrowth
-		GetSpellInfo(100977), -- Harmony 
+		GetSpellInfo(100977), -- Harmony
 		GetSpellInfo(158792), -- Pulverize
 		GetSpellInfo(155777), -- Rejuv (Germination)
+		GetSpellInfo(191034), -- Starfall
 		(GetSpellInfo(774)), -- Rejuvenation --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	DOTs = {
 		GetSpellInfo(339), -- Entangling Roots
 		GetSpellInfo(2637), -- Hibernate
 		GetSpellInfo(164815), -- Sunfire
-		GetSpellInfo(115798), -- Weakened Blows 
+		GetSpellInfo(115798), -- Weakened Blows
+		GetSpellInfo(202347), -- Stellar Flare
 		(GetSpellInfo(164812)), -- Moonfire --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Feral = {
@@ -28,11 +30,8 @@ return {
 		GetSpellInfo(52610), -- Savage Roar
 		GetSpellInfo(5211), -- Bash
 		GetSpellInfo(5211), -- Dash
-		GetSpellInfo(5229), -- Enrage
 		GetSpellInfo(22842), -- Frenzied Regeneration
-		GetSpellInfo(33745), -- Lacerate
 		GetSpellInfo(22570), -- Maim
-		GetSpellInfo(9007), -- Pounce Bleed
         GetSpellInfo(77758), -- Thrash
         GetSpellInfo(78892), -- Stampede
         GetSpellInfo(77761), -- Stampeding Roar
@@ -42,19 +41,14 @@ return {
 		(GetSpellInfo(5217)), -- Tiger's Fury --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Talents = {
-		GetSpellInfo(58181), -- Infected Wounds
 		GetSpellInfo(50334), -- Berserk
-		GetSpellInfo(16850), -- Celestial Focus
 		GetSpellInfo(16857), -- Faerie Fire (Feral)
 		GetSpellInfo(16979), -- Feral Charge - Bear
 		GetSpellInfo(33831), -- Force of Nature
 		GetSpellInfo(33878), -- Mangle (Bear)
 		GetSpellInfo(33876), -- Mangle (Cat)
 		GetSpellInfo(48438), -- Wild Growth
-		GetSpellInfo(48517), -- Eclipse (Solar)
-		GetSpellInfo(48518), -- Eclipse (Lunar)
         GetSpellInfo(69369), -- Predator's Swiftness
-		GetSpellInfo(108294), -- Heart of the Wild
 		GetSpellInfo(124974), -- Nature's Vigil
 		GetSpellInfo(106922), -- Might of Ursoc
 		GetSpellInfo(102558), -- Incarnation: Sun of Ursoc
